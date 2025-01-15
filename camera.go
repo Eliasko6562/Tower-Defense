48baa0a890705f3e79a0b2ad815bcd057efd7b7c
Eliasko6562 <eliascomastantine@gmail.com>
2025-01-15 19:10:33 +0100

    max and min zoom level

--- camera.go
+++ camera.go
@@ -16,43 +16,54 @@ func (g *game) init() {
 	g.camera.Up = rl.NewVector3(0.0, 1.0, 0.0)
 	g.camera.Fovy = 10.0
 	g.camera.Projection = rl.CameraOrthographic
-	g.cameraSpeed = 1.0
+	g.cameraSpeed = 0.075
 }
 
 func (g *game) update() {
+	finalSpeed := g.cameraSpeed * g.camera.Fovy
 	// Update the camera.
 	if rl.IsKeyDown(rl.KeyW) {
-		g.camera.Position.X += g.cameraSpeed
-		g.camera.Target.X += g.cameraSpeed
-		g.camera.Position.Z += g.cameraSpeed
-		g.camera.Target.Z += g.cameraSpeed
+		g.camera.Position.X -= finalSpeed
+		g.camera.Target.X -= finalSpeed
+		g.camera.Position.Z -= finalSpeed
+		g.camera.Target.Z -= finalSpeed
 	}
 	if rl.IsKeyDown(rl.KeyS) {
-		g.camera.Position.X -= g.cameraSpeed
-		g.camera.Target.X -= g.cameraSpeed
-		g.camera.Position.Z -= g.cameraSpeed
-		g.camera.Target.Z -= g.cameraSpeed
+		g.camera.Position.X += finalSpeed
+		g.camera.Target.X += finalSpeed
+		g.camera.Position.Z += finalSpeed
+		g.camera.Target.Z += finalSpeed
 	}
 	if rl.IsKeyDown(rl.KeyA) {
-		g.camera.Position.X -= g.cameraSpeed
-		g.camera.Target.X -= g.cameraSpeed
-		g.camera.Position.Z += g.cameraSpeed
-		g.camera.Target.Z += g.cameraSpeed
+		g.camera.Position.X -= finalSpeed
+		g.camera.Target.X -= finalSpeed
+		g.camera.Position.Z += finalSpeed
+		g.camera.Target.Z += finalSpeed
 	}
 	if rl.IsKeyDown(rl.KeyD) {
-		g.camera.Position.X += g.cameraSpeed
-		g.camera.Target.X += g.cameraSpeed
-		g.camera.Position.Z -= g.cameraSpeed
-		g.camera.Target.Z -= g.cameraSpeed
+		g.camera.Position.X += finalSpeed
+		g.camera.Target.X += finalSpeed
+		g.camera.Position.Z -= finalSpeed
+		g.camera.Target.Z -= finalSpeed
 	}
-	if rl.GetMouseWheelMove() != 0 {
-		g.camera.Position.X += g.cameraSpeed
-		g.camera.Target.X += g.cameraSpeed
-		g.camera.Position.Y += g.cameraSpeed
-		g.camera.Target.Y += g.cameraSpeed
-		g.camera.Position.Z += g.cameraSpeed
-		g.camera.Target.Z += g.cameraSpeed
-		g.camera.Fovy += g.cameraSpeed
+	if scroll := rl.GetMouseWheelMove(); scroll != 0 {
+		if scroll > 0 && g.camera.Fovy > 3 {
+			g.camera.Position.X -= finalSpeed
+			g.camera.Target.X -= finalSpeed
+			g.camera.Position.Y -= finalSpeed
+			g.camera.Target.Y -= finalSpeed
+			g.camera.Position.Z -= finalSpeed
+			g.camera.Target.Z -= finalSpeed
+			g.camera.Fovy -= finalSpeed
+		} else if scroll < 0 && g.camera.Fovy < 100 {
+			g.camera.Position.X += finalSpeed
+			g.camera.Target.X += finalSpeed
+			g.camera.Position.Y += finalSpeed
+			g.camera.Target.Y += finalSpeed
+			g.camera.Position.Z += finalSpeed
+			g.camera.Target.Z += finalSpeed
+			g.camera.Fovy += finalSpeed
+		}
 	}
 
 }
