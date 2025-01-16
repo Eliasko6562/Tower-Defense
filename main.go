ba9c5c3e4fe6f24c3059fd7fb2cd39163af2f8ab
Elias Constantine <147802118+Eliasko6562@users.noreply.github.com>
2025-01-16 12:34:10 +0000

    added texture to cube

--- main.go
+++ main.go
@@ -2,14 +2,20 @@ package main
 
 import rl "github.com/gen2brain/raylib-go/raylib"
 
-
-
 func main() {
 	rl.InitWindow(800, 450, "raylib [core] example - basic window")
 	defer rl.CloseWindow()
 
 	game := game{}
 	game.init()
+
+	mesh := rl.GenMeshCube(1.0, 1.0, 1.0)
+	model := rl.LoadModelFromMesh(mesh)
+
+	image := rl.GenImageWhiteNoise(128, 128, 16)
+	texture := rl.LoadTextureFromImage(image)
+	rl.UnloadImage(image)
+	model.Materials.Maps.Texture = texture
 
 	rl.SetTargetFPS(60)
 
@@ -21,21 +27,13 @@ func main() {
 
 		rl.ClearBackground(rl.RayWhite)
 		rl.BeginMode3D(game.camera)
-		
 
-
-		rl.DrawCube(rl.NewVector3(0, 0, 0), 2, 2, 2, rl.Red)
-		rl.DrawCubeWires(rl.NewVector3(0, 0, 0), 2, 2, 2, rl.Maroon)
+		rl.DrawModel(model, rl.NewVector3(0, 0, 0), 1, rl.Red)
 		rl.DrawGrid(10, 1.0)
-
-
-
-
-
-
-
 
 		rl.EndMode3D()
 		rl.EndDrawing()
 	}
+
+	rl.UnloadTexture(texture)
 }
